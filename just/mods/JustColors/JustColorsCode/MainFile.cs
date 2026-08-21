c5b7bbe5be0751e2d4526ce1f05a35058a5b7be5
jane <yubin.yumiko.ichikawa@gmail.com>
2026-08-20 22:40:20 -0400

    fixed the thing

--- just/mods/JustColors/JustColorsCode/MainFile.cs
+++ just/mods/JustColors/JustColorsCode/MainFile.cs
@@ -6,7 +6,7 @@ using BaseLib.Config;
 namespace JustColors.JustColorsCode {
   [ModInitializer(nameof(Initialize))]
   public partial class MainFile : Node {
-      public const string ModId = "just_colors";
+      public const string ModId = "JustColors";
 
       public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
           new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
